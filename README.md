c51092ac9a9940f18f4deda4545a2f869ee3ca8e
PixelTwitt <pixeltwitt@gmail.com>
2024-11-19 09:44:40 +0100

    Primera prueba

---
+++ README.md
@@ -0,0 +1,3 @@
+# **_Proyecto ed_ut5_JonathanMartínezPagan_**
+
+Proyecto de trabaja de Entornos de desarrollo y subido a GitHub
---
+++ src/com/jonmarti/ed_ut5_proyecto/edUt5JonathanMartinez.java
@@ -0,0 +1,51 @@
+package com.jonmarti.ed_ut5_proyecto;
+
+/*
+ * Programa que solicita al usuario día y mes del año y muestra si fecha correcta
+ */
+import java.util.Scanner;
+
+public class edUt5JonathanMartinez {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); // Llamamos a Scanner para entrada en teclado
+        int mes;
+        int numDias;
+        // Se pide la entrada de datos desde teclado
+        System.out.print("Introduzca día: ");
+        numDias = sc.nextInt();
+
+        System.out.print("Introduzca mes: ");
+        mes = sc.nextInt();
+
+        boolean fecha = false; // Se verifica si fecha correcta
+
+        if (mes >= 1 && mes <= 12) {
+            switch (mes) { //iniciamos la instruccion switch para múltiples alternativas
+                case 4: case 6: case 9: case 11: // Asignamos los meses de 30 días
+                    if (numDias >= 1 && numDias <= 30) {
+                        fecha = true;
+                    }
+                    break;
+                case 2: //Caso especial mes febrero con 28 días
+                    if (numDias >= 1 && numDias <= 28) {
+                        fecha = true;
+                    }
+                    break;
+                default: // Resto de meses a 31 días
+                    if (numDias >= 1 && numDias <= 31) {
+                        fecha = true;
+                    }
+                    break;
+            }
+        }
+        // Imprime en pantalla resultado
+        if (fecha) {
+            System.out.print("Fecha OK: " + numDias + "/" + mes);
+        }
+        else {
+            System.out.print("Fecha incorrecta ");
+        }
+
+
+    }
+}
